33482aa8a5554f76bff243e0e6c5de03b0c20f05
McShi <rashida.mcwilliams@gmail.com>
2017-10-26 16:01:43 -0400

    Adjusted database

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -13,12 +13,14 @@ namespace Bookshelf.Models
         public int ID { get; set; }
         [MinLength(2), MaxLength(50)]
         public string Title { get; set; }
+        [Display(Name ="Publishd Date")]
         public DateTime PublishedDate { get; set; }
         [MaxLength(500)]
         public string Description { get; set; }
+        //[Required]
         public string ISBN { get; set; }
 
-        [ForeignKey("Category")]
+        [ForeignKey("Category"), Display(Name="Category")]
         public int CategoryID { get; set; }
         public virtual Category Category { get; set; }
     }
